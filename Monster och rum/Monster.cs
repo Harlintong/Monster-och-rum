96b6e4bfbb70b11b0267e567daaa153e6ec10ebc
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2020-02-18 12:47:08 +0100

    Add project files.

---
+++ Monster och rum/Monster.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monster_och_rum
+{
+    class Monster
+    {
+    }
+}
---
+++ Monster och rum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monster_och_rum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
