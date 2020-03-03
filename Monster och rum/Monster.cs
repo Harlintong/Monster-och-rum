06cc79c3a453d44e8ccf580e083e6230a0af45f7
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2020-03-03 12:50:39 +0100

    k

--- Monster och rum/Monster.cs
+++ Monster och rum/Monster.cs
@@ -6,5 +6,38 @@ namespace Monster_och_rum
 {
     class Monster
     {
+        List<int> Damage = new List<int>();
+        Random Generator = new Random();
+
+        private int hitpoints;
+        private int xp;
+
+        public Monster()
+        {
+            hitpoints = 100;
+
+        }
+
+        public void Hurt(int x)
+        {
+            hitpoints = hitpoints - x;
+        }
+
+        public void Attack(int x)
+        {
+            x = x - ; 
+        }
+
+        public bool IsAlive()
+        {
+            if(hitpoints <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
